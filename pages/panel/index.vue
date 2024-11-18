63fec76979c8153c21585bbed73f14409e14f8eb
mohammad <69156294+mohammad-ashrafi2020@users.noreply.github.com>
2024-11-18 20:59:33 +0330

    fix css and create panel layout

---
+++ pages/panel/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="flex gap-4">
+      <h4>آخرین سفارش‌ها</h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "panel"
+})
+</script>
+
+<style></style>
